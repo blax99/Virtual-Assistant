368b037adf174c24355b52f714e763e5c65290b2
Krrish Chaudhary <chaudhariikrishna1@gmail.com>
2026-04-15 22:23:36 +0545

    feat: create authentication API structure

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDb from './config/db.js'
+import authRouter from './routes/auth.routes.js'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 dotenv.config()
+
+app.use("/api/auth",authRouter)
+app.use(express.json())
+app.use(cookieParser())
 
 const port = process.env.PORT || 5000
 app.get("/",(req, res)=>{
     res.send('hello!')
 })
+
 
 app.listen(port, ()=>{
     connectDb()
